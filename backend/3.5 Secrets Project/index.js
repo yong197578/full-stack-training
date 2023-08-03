4cf6a48b7271a84971c38710e1aeec91b4ae61af
Yong Choi <yong.s.choi2@gmail.com>
2023-08-03 16:24:32 -0500

    express.js with node.js

---
+++ backend/3.5 Secrets Project/index.js
@@ -0,0 +1,41 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+let isAuthorized = false;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+function secret (req, res, next){
+    let password = req.body["password"];
+    if(password === "ILoveProgramming"){
+        isAuthorized = true;
+    }
+    next();
+}
+app.use(secret);
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post("/check", (req, res) =>{
+    if(isAuthorized){
+        res.sendFile(__dirname + "/public/secret.html")
+    }else {
+        // res.sendFile(__dirname+ "/public/index.html");
+        res.redirect("/");
+    }
+});
+
+app.listen(port, ()=>{
+    console.log(`Local Port ${port}`);
+})
